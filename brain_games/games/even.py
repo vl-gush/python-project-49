a4009d5293af50aaa1cbdc61c015c2d81afd46a8
Влад Гуштын <vladislav3372@gmail.com>
2022-12-04 16:31:05 +0300

    Fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,4 +1,9 @@
-def even(number: int) -> bool:
-    """Returns True if the number is even, otherwise False.
+import random
+
+
+def even() -> str:
+    """Returns "yes" if the number is even, otherwise "no".
     """
-    return number % 2 == 0
+    number = random.randint(0, 100)
+    print(f"Question: {number}")
+    return "yes" if number % 2 == 0 else "no"
