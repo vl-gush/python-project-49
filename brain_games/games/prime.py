c06bed53b63daa88d20201c88711d60200cea8ff
Влад Гуштын <vladislav3372@gmail.com>
2023-06-26 21:09:26 +0300

    Fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -13,7 +13,7 @@ def prime():
 
 
 def is_prime(number):
-    for i in range(2, math.ceil(math.sqrt(number))):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
     return True
